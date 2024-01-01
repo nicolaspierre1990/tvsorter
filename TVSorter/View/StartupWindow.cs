0802ba877645d8a6c06e07c705f5ca7b9cd575ce
Nicolas Pierre <nico_kestens@hotmail.com>
2024-01-02 00:45:59 +0100

    update splash screen

--- TVSorter/View/StartupWindow.cs
+++ TVSorter/View/StartupWindow.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.ComponentModel;
+using System.Windows.Forms;
 
 namespace TVSorter.View;
 
@@ -11,5 +12,41 @@ public partial class StartupWindow : Form
         this.pictureBox1.Width = 500;
         this.pictureBox1.Height = 100;
         this.pictureBox1.Image = new System.Drawing.Bitmap(Resources.Resources.logo_no_background, 500, 100);
+
+        var backgroundWorker = new BackgroundWorker
+        {
+            WorkerReportsProgress = true,
+            WorkerSupportsCancellation = true
+        };
+
+        backgroundWorker.DoWork += BackgroundWorker_DoWork;
+        backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+
+        backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+
+        backgroundWorker.RunWorkerAsync();
+    }
+
+    private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+        MainForm mainForm = new();
+        mainForm.Show();
+
+        this.Hide();
+    }
+
+    private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+    {
+       this.progressBar1.Value = e.ProgressPercentage;
+    }
+
+    private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+    {
+        BackgroundWorker worker = sender as BackgroundWorker;
+
+        for (int i = 0; i < 10; i++)
+        {
+            worker.ReportProgress(10 * (i + 1));
+        }
     }
 }
